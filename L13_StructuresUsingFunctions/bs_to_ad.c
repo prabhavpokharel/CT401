ed14470e01dfff0da53d126b55faf6dca04fe479
prabhavpokharel <akacaptainfury@gmail.com>
2023-08-05 12:29:16 +0545

    change bs pointer to variable

--- L13_StructuresUsingFunctions/bs_to_ad.c
+++ L13_StructuresUsingFunctions/bs_to_ad.c
@@ -6,7 +6,7 @@ struct date
 	int day, month, year;
 };
 
-void conv(struct date *, struct date *);
+void conv(struct date, struct date *);
 
 void main()
 {
@@ -15,29 +15,29 @@ void main()
 	printf("Enter date in BS (YYYY MM DD): ");
 	scanf("%d%d%d", &bs.year, &bs.month, &bs.day);
 	
-	conv(&bs, &ad);
+	conv(bs, &ad);
 	
 	printf("Date (in AD): %d-%d-%d", ad.year, ad.month, ad.day);
 	
 	getch();
 }
 
-void conv(struct date *bs, struct date *ad)
+void conv(struct date bs, struct date *ad)
 {
-	ad->day = bs->day - 16;
+	ad->day = bs.day - 16;
 	if(ad->day<=0)
 	{
 		ad->day += 30;
-		bs->month -= 1;
+		bs.month -= 1;
 	}
 	
-	ad->month = bs->month - 8;
+	ad->month = bs.month - 8;
 	if(ad->month<=0)
 	{
 		ad->month += 12;
-		bs->year -= 1;
+		bs.year -= 1;
 	}
 	
-	ad->year = bs->year - 56;
+	ad->year = bs.year - 56;
 }
 
